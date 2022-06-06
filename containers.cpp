fa65e5a562110a56e35e4fe3f891423f8e929220
Daniel Rector <drector@vmware.com>
2022-06-05 21:49:56 -0400

    first commit

---
+++ containers.cpp
@@ -0,0 +1,34 @@
+#include "Resource.h"
+#include <vector>
+#include <stdexcept>
+#include <iostream>
+
+using std::vector;
+using std::cout;
+
+
+int main()
+{
+    vector numbers{ 0,1,2 };
+    numbers.push_back(-2);
+    numbers[0] = 3;
+    int num = numbers[2];
+
+    for (int i : numbers)
+    {
+        cout << i << '\n';
+    }
+
+    Resource r("local");
+    {
+        cout << "-----------------" << '\n';
+        vector<Resource> resources;
+        resources.push_back(r);
+        cout << "-----------------" << '\n';
+        resources.push_back(Resource("first"));
+        cout << "-----------------" << '\n';
+        resources.push_back(Resource("second"));
+        cout << "-----------------" << '\n';
+    }
+    return 0;
+}
---
+++ resource.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+class Resource
+{
+private:
+    std::string name;
+public:
+    Resource(std::string n);
+    Resource(const Resource& r);
+    ~Resource(void);
+    std::string GetName() const { return name; }
+};
